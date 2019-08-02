299aa4c933561a203d48713ee8f437964d56b76d
Joel Rosdahl <joel@rosdahl.net>
2019-08-02 20:37:57 +0200

    Fix comparison with the “cu” language
    This should have been done in c4fffda0.
    
    Related to PR #381.

--- src/ccache.c
+++ src/ccache.c
@@ -2143,7 +2143,7 @@ calculate_object_hash(struct args *args, struct hash *hash, int direct_mode)
 		hash_delimiter(hash, "/dev/null dependency file");
 	}
 
-	if (!found_ccbin && str_eq(actual_language, "cuda")) {
+	if (!found_ccbin && str_eq(actual_language, "cu")) {
 		hash_nvcc_host_compiler(hash, NULL, NULL);
 	}
 
